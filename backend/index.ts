d55605162c9d9664192780f250f09e3cce752b5a
SanqDuonq <namsang0902s@gmail.com>
2024-12-03 14:15:10 +0700

    Connected database

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
-
+import { connectDB } from './utils/connect-database';
+dotenv.config();
 const app = express();
-const port = 8888;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(cookieParser());
@@ -14,5 +14,7 @@ app.use(express.json());
 
 app.listen(port,() => {
     console.log(`App started at http://localhost:${port}`)
+    connectDB();
 })
+
 
